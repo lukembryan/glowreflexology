190d866b2108027f7230c9c139d71f12b06cd38c
Luke Bryan <luke@lukembryan.com>
2019-05-01 20:26:26 +0100

    release 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { ConfigService } from '../app/config.service';
 
+declare let gtag: Function;
+
 @Component({
   selector: '#root',
   templateUrl: './app.component.html',
@@ -18,29 +20,41 @@ export class AppComponent implements OnInit {
   previousUrl:string;
 
   constructor(private config: ConfigService, private router: Router, private renderer: Renderer2) {
+    this.dev =  ['localhost', 'local.glowreflexology.co.uk'].indexOf(config.nativeWindow.location.hostname) >= 0;
+
     router.events.subscribe((event) => {
       if(event instanceof NavigationEnd){
         this.config.setRoute(event.url);
         this.config.toggleMenu(false);
 
+        if(!this.dev){
+          gtag('config', 'UA-20448588-9', {
+            'page_title' : 'glow reflexology',
+            'page_path': event.urlAfterRedirects
+          });
+        }
+
         this.renderer.removeClass(document.body, (this.previousUrl ? this.previousUrl : 'home'));
         let currentUrlSlug = event.url.slice(1)
         this.renderer.addClass(document.body, (currentUrlSlug ? currentUrlSlug : 'home'));
         this.previousUrl = currentUrlSlug;
-        console.log(currentUrlSlug);
         this.scrolledBottom = currentUrlSlug == '' ? true : false;
       }
     });
-
-    this.dev = config.nativeWindow.location.hostname == 'localhost';
   }
 
-  ngOnInit() {
+  ngOnInit(){
     this.config.currentRoute.subscribe(url => this.route = url.slice(1) ? url.slice(1) : 'home');
   }
 
   onScroll(e){
     this.scrolledDown = e.target.scrollTop >= 10;
-    this.scrolledBottom = (e.target.scrollHeight - e.target.clientHeight - e.target.scrollTop) < 10;
+    if(this.route == 'home') this.scrolledBottom = true;
+    else this.scrolledBottom = (e.target.scrollHeight - e.target.clientHeight - e.target.scrollTop) < 10;
   };
+
+  onActivate(e){
+    let template = document.querySelector('#template');
+    template.scrollTo(0,0);
+  }
 }
